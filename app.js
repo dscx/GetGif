ffd4fc20aeff2d878d519946d92b59e957bc9c3a
Dan <dnc223@gmail.com>
2014-08-27 03:09:58 -0400

    Create website

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var http = require('http');
+var ejs = require('ejs');
+var url = require('url');
+var request = require('request');
+var fs = require('fs');
+
+
+
+
+var app = express();
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.get('/gif/*', function(req, res) {
+  var path = url.parse(req.url).pathname;
+  var terms = path.substring(5).split("/").join("+");
+  var terms = "http://api.giphy.com/v1/gifs/search?q=" + terms + "&api_key=dc6zaTOxFJmzC&limit=1";
+
+  request(terms, function(error, response, body) {
+    try {
+      var image = JSON.parse(body).data[0].images.original;
+      console.log(image.url);
+      res.render('layout.ejs', {imageURL : image.url});
+    } catch (err) {
+      res.render('layout.ejs', {imageURL : '/default.gif'});
+    }
+  });
+});
+
+
+
+
+var server = app.listen(3000, function() {
+  console.log("Listening on port %d", server.address().port);
+});
