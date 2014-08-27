5bccb99a509cf4a8da92dabe81bf2a5abf4d4fd4
Dan <dnc223@gmail.com>
2014-08-27 11:24:43 -0400

    test

--- app.js
+++ app.js
@@ -34,22 +34,22 @@ app.get('/*', function(req, res) {
   request(stuff, function(error, response, body) {
     try {
       var image = JSON.parse(body).data[0].images.original;
+      // http.get(image.url).on('response', function (response) {
       if (gif === true) {
-        //res.writeHead(301,{'Content-Type':'text/html', 'Location': image.url});
-        http.get(image.url).on('response', function (response) {
-          response.on('data', function(chunk) {
-            res.write(chunk);
-          });
-          response.on('end', function() {
-            res.end();
-          });
-        });
+        res.writeHead(301,{'Content-Type':'text/html', 'Location': image.url});
       } else {
         var loc = 'http://getgif.azurewebsites.net/gif/' + path + '.gif';
         console.log('LOC', loc);
         res.writeHead(301,{'Content-Type':'text/html', 'Location': loc});
       }
       res.end();
+      //   response.on('data', function(chunk) {
+      //     res.write(chunk);
+      //   });
+      //   response.on('end', function() {
+      //     res.end();
+      //   });
+      // });
     } catch (err) {
       res.writeHead(301,{'Content-Type':'text/html', 'Location': 'http://media4.giphy.com/media/zLCiUWVfex7ji/giphy.gif'});
       res.end();
