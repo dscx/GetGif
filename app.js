c953ff3f81896a372003f9b4625c98e6fb59e630
Casey Garland <caseyg1204@gmail.com>
2014-08-28 23:45:39 -0400

    azure troubleshooting

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ else{
 }
 // var auth = require('http-auth');
 console.log(__dirname, 'diranme');
+fs.readdir(__dirname, function(err, files){
+  console.log(files);
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
